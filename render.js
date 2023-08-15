1bc1d2fb4cd0f1e92250aed979eb2f9e72720204
lukejoliat <lukejoliat@gmail.com>
2023-08-15 12:03:33 -0400

    Styles

--- render.js
+++ render.js
@@ -1,36 +1,40 @@
 const t = todos();
 
-const deleteTodo = (id) => {
-  t.deleteTodo(id);
-  renderTodos();
-};
-const completeTodo = (id) => {
-  t.completeTodo(id);
-  renderTodos();
-};
-const saveBtn = document.querySelector(".todos_save");
-const todosElement = document.querySelector(".todos_list");
+const renderer = (f) =>
+  (newFunc = (...params) => {
+    f(...params);
+    renderTodos();
+  });
 
-saveBtn.addEventListener("click", (e) => {
+const deleteTodo = renderer(t.deleteTodo);
+const completeTodo = t.completeTodo;
+const saveTodo = renderer((e) => {
   const input = document.querySelector(".todos_input");
   t.addTodo(input.value);
   input.value = "";
-  renderTodos();
 });
+
+const saveBtn = document.querySelector(".todos_save");
+const saveInput = document.querySelector(".todos_input");
+const todosElement = document.querySelector(".todos_list");
+saveBtn.addEventListener("click", saveTodo);
+saveInput.addEventListener("keydown", (e) =>
+  e.code === "Enter" ? saveTodo() : null
+);
 
 const renderTodos = () => {
   todosElement.innerHTML = ``;
   const todos = t.getTodos() ?? [];
   todos.forEach((t, i) => {
     todosElement.innerHTML += `
-      <input class="todo" type="checkbox" id="todos_${i}" name="todos_${i}" onClick="completeTodo(${
+    <div class="todo_row">
+        <input class="todo todo_check" type="checkbox" id="todo_${i}" name="todo_${i}" onClick="completeTodo(${
       t.id
     })" value="${t.name}" ${t.completed ? "checked" : null} />
-      <label for="todos_${i}">${
+        <label for="todo_${i}" class="todo_label">${
       t.name
-    }</label><button class="todo_delete" onClick="deleteTodo(${
-      t.id
-    })">x</button><br />
+    }</label><button class="todo_delete" onClick="deleteTodo(${t.id})"></button>
+    </div>
     `;
   });
 };
